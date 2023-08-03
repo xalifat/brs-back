87465dacab8768ac8f31c2c0be83ab922eef29a7
Adam <adambatukaev2003@mail.ru>
2023-08-03 15:35:56 +0300

    авторизация

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use('/users', usersRouter);
+app.use(usersRouter);
 
 
 mongoose
-  .connect(process.env.MONGO)
+  .connect(process.env.MONGO_URL)
   .then(() => console.log("Успешно соединились с сервером MongoDB"))
   .catch(() => console.log("Ошибка при соединении с сервером MongoDB"));
 
